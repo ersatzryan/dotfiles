e549d0b22f8b7ab0437e34c0466448fab9f2209b
Ryan G <ersatzryan@gmail.com>
2013-02-20 21:16:11 -0500

    Add Bash Aliases

---
+++ bash/aliases/rails.bash
@@ -0,0 +1,3 @@
+alias rc='rails c'
+alias rm='rake db:migrate db:test:prepare'
+alias rs='rails s'
---
+++ bash/aliases/tmux.bash
@@ -0,0 +1,3 @@
+alias tap='tpr attach -t pair'
+alias tnp='ssh-agent tpr new -s pair'
+alias tpr='tmux -S /User/Share/.tmux'
