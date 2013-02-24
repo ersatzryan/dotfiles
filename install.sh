a4313e1f29bc9f666e37f53dac095e35c6461ffb
Ryan Glover <ersatzryan@gmail.com>
2013-02-24 16:37:29 -0500

    Fix install script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 for f in `find $( dirname "$BASH_SOURCE[0]" ) -name '*.ln'`; do
   filename=$(basename "$f")
-  ln -s "$f" "~/.${filename%.ln}"
+  if [ -e "$HOME/.${filename%.ln}" ]
+  then
+    rm "$HOME/.${filename%.ln}"
+  fi
+
+  ln -s "$( greadlink -f "$f" )" "$HOME/.${filename%.ln}"
 done
