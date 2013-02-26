f8f3476d579c138b2ceef1c840158c3377a14149
Ryan Glover <ersatzryan@gmail.com>
2013-02-26 17:46:49 -0500

    Use old tmux socket style

--- bash/aliases/tmux.bash
+++ bash/aliases/tmux.bash
@@ -1,3 +1,2 @@
-alias tap='tpr attach -t pair'
-alias tnp='ssh-agent tpr new -s pair'
-alias tpr='tmux -S /User/Share/.tmux'
+alias tap='tmux -S /tmp/pair attach -t pair'
+alias tnp='ssh-agent tmux -S /tmp/pair new -s pair'
