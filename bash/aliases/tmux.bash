e9cc56850f4622da758fb0fb687ec7aa248dd617
Ryan Glover <ersatzryan@gmail.com>
2013-12-23 19:48:03 -0500

    Add completion to ta (tmux attach) for session names

--- bash/aliases/tmux.bash
+++ bash/aliases/tmux.bash
@@ -5,3 +5,11 @@ alias tap='ta pair'
 alias tnp='ssh-agent tn pair'
 alias tls='t list-sessions'
 alias trm='t kill-session -t'
+
+function _tmux_complete_session() {
+    local IFS=$'\n'
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    COMPREPLY=( ${COMPREPLY[@]:-} $(compgen -W "$(tls | cut -f 1 -d ':')" -- "${cur}") )
+}
+
+complete -F _tmux_complete_session ta
