2a1b33288b6e1fd6860c0a95fa45b10113441404
Ryan Glover <ersatzryan@gmail.com>
2013-12-26 13:59:11 -0500

    Install brew and install from brewfile in home dir

--- install.sh
+++ install.sh
@@ -8,4 +8,9 @@ for f in `find $( dirname "$BASH_SOURCE[0]" ) -name '*.ln'`; do
   ln -s "$( greadlink -f "$f" )" "$HOME/.${filename%.ln}"
 done
 
+if type foo >/dev/null 2>&1; then
+  ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+  brew bundle ~/.brewfile
+fi
+
 vim -u ~/.vim/bundles.vim +BundleInstall +qall
